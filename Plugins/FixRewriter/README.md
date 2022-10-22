fa32e83930d1388d430fd4695f17e6bf63786dcc
NikoDervaric <nikodervaric@gmail.com>
2022-10-22 18:22:30 +0200

    Added FixRewriter and direct command to BasicIcao.

---
+++ Plugins/FixRewriter/README.md
@@ -0,0 +1,12 @@
+﻿# FIX Rewriter
+
+This plugin rewrites FIXs stored in the `fixes.fix` file and rewrites the text to a coordinate. FIXs should be stored in the file in the following format.
+
+Example:
+```
+<FIX 1> <lat1>;<lon1>;
+<FIX 2> <lat2>;<lon2>;
+<FIX 3> <lat3>;<lon3>;
+```
+
+Maintainers: Niko (639233) & Wes (644899)
---
+++ Plugins/FixRewriter/Rewriter.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+using TrainingServer;
+using TrainingServer.Extensibility;
+
+namespace FixRewriter;
+
+public class Rewriter : IRewriter
+{
+#if DEBUG
+    public string FriendlyName => "FIX Rewriter (DEBUG)";
+#else
+    public string FriendlyName => "FIX Rewriter";
+#endif
+    public string Maintainer => "Niko (639233)";
+
+    public Regex Pattern { get; }
+
+    private readonly Dictionary<string, string> _rewrites = new();
+
+    public Rewriter()
+    {
+        if (!File.Exists("fixes.fix"))
+            File.WriteAllText("fixes.fix", string.Empty);
+
+        foreach (string l in File.ReadAllLines("fixes.fix").Select(l => l.Trim()))
+        {
+            int splitPoint = l.IndexOf(' ');
+            if (splitPoint < 0)
+            {
+                Console.Error.WriteLine("Could not find fix from line: " + l);
+                continue;
+            }
+
+            _rewrites.Add(l[..splitPoint], l[(splitPoint + 1)..].Trim());
+        }
+        Pattern = new(string.Join('|', _rewrites.Keys.Select(k => Regex.Escape(k))), RegexOptions.CultureInvariant | RegexOptions.Compiled);
+    }
+
+    public string Rewrite(string message)
+    {
+        foreach (var kvp in _rewrites)
+            message = message.Replace(kvp.Key, kvp.Value);
+
+        return message;
+    }
+}
